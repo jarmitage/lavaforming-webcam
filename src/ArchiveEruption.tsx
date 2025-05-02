29982fa395caca60f3ef37633421ce10fa6c7226
Jack Armitage <jack.armitage@me.com>
2025-05-02 13:26:21 +0000

    fix loop

--- src/ArchiveEruption.tsx
+++ src/ArchiveEruption.tsx
@@ -1,34 +1,66 @@
-import YouTube from 'react-youtube'
+import YouTube, { YouTubeProps, YouTubeEvent } from 'react-youtube'
 import styles from './styles/ArchiveEruption.module.css';
+import React from 'react';
 
 const id = 'qkk-zwMQiBc'
 
-function ArchiveEruption() {
+interface YouTubePlayerState {
+  player: YouTubeEvent['target'] | null;
+}
 
-  const opts = {
-    width: '100%',
-    height: '100%',
-    playerVars: {
-      autoplay: 1,
-      mute: 1,
-      controls: 0,
-      modestbranding: 1,
-      rel: 0,
-      showinfo: 0,
-      loop: 1
+class YouTubePlayer extends React.Component<Record<string, never>, YouTubePlayerState> {
+  constructor(props: Record<string, never>) {
+    super(props)
+    this.state = {
+      player: null
     }
   }
 
-  return (
-    <div>
-      <h1>Archive Eruption Page</h1>
-      <YouTube
-        videoId={id}
-        opts={opts}
-        className={styles.youtubePlayer}
-      />
-    </div>
-  )
+  onReady: YouTubeProps['onReady'] = (event: YouTubeEvent) => {
+    console.log('[ArchiveEruption.onReady] Video ready', event)
+    this.setState({ player: event.target })
+    event.target.playVideo()
+  }
+
+  // onEnd: YouTubeProps['onEnd'] = (event: YouTubeEvent) => {
+  //   console.log('[ArchiveEruption.onEnd] Video ended', event)
+  //   this.restart()
+  // }
+
+  restart = () => {
+    console.log('[ArchiveEruption.restart] Restarting video')
+    if (this.state.player) {
+      this.state.player.seekTo(0)
+      this.state.player.playVideo()
+    }
+  }
+
+  render() {
+    const opts = {
+      width: '100%',
+      height: '100%',
+      playerVars: {
+        autoplay: 1,
+        mute: 1,
+        controls:0,
+        modestbranding: 1,
+        rel: 0,
+        showinfo: 0,
+        loop: 1
+      }
+    }
+    return (
+      <div>
+        <YouTube
+          videoId={id}
+          opts={opts}
+          className={styles.youtubePlayer}
+          onReady={this.onReady}
+          onEnd={this.restart}
+        />
+      </div>
+    )
+  }
 }
 
-export default ArchiveEruption; 
+export default YouTubePlayer; 
